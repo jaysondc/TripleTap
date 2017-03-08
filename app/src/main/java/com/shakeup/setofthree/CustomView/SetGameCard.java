ef12ca030f4cff8790015ba05de2e2a1133c2f04
Jayson Dela Cruz <jayson.p.delacruz@gmail.com>
2017-03-08 12:57:05 -0800

    SetGameCard now displays a placeholder image programmatically.

--- app/src/main/java/com/shakeup/setofthree/CustomView/SetGameCard.java
+++ app/src/main/java/com/shakeup/setofthree/CustomView/SetGameCard.java
@@ -4,6 +4,12 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.shakeup.setofthree.R;
 
@@ -31,6 +37,11 @@ public class SetGameCard extends CardView{
     private int mCount;
     private int mFill;
 
+    /**
+     * Keep an reference to the view's context
+     */
+    private Context mContext;
+
     public SetGameCard(Context context){
         super(context);
     }
@@ -43,71 +54,197 @@ public class SetGameCard extends CardView{
      */
     public SetGameCard(Context context, AttributeSet attrs) {
         super(context, attrs);
-
+        mContext = context;
         init(context, attrs);
     }
 
     public SetGameCard(Context context, AttributeSet attrs, int defStyle)
     {
         super(context, attrs, defStyle);
-
+        mContext = context;
         init(context, attrs);
     }
 
     /**
      * Initialize our card with passed in attributes.
-     * @param context
-     * @param attrs
+     * @param context Context our view is in
+     * @param attrs Attributes passed into the constructor from code or the layout xml
      */
     private void init(Context context, AttributeSet attrs)
     {
+
+        /**
+         * Get custom attributes
+         */
         TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.SetGameCard);
 
-        mAspectRatioWidth = attributes.getInt(R.styleable.SetGameCard_aspectWidth, 2);
-        mAspectRatioHeight = attributes.getInt(R.styleable.SetGameCard_aspectHeight, 3);
+        try {
+            mAspectRatioWidth = attributes.getInt(R.styleable.SetGameCard_aspectWidth, 2);
+            mAspectRatioHeight = attributes.getInt(R.styleable.SetGameCard_aspectHeight, 3);
 
-        // Defaults to Oval:Red:One:Solid if not set
-        mShape = attributes.getInt(R.styleable.SetGameCard_symbol_shape, 0);
-        mColor = attributes.getInt(R.styleable.SetGameCard_symbol_color, 0);
-        mCount = attributes.getInt(R.styleable.SetGameCard_symbol_count, 0);
-        mFill = attributes.getInt(R.styleable.SetGameCard_symbol_fill, 0);
+            // Defaults to Oval:Red:One:Solid if not set
+            mShape = attributes.getInt(R.styleable.SetGameCard_symbol_shape, 0);
+            mColor = attributes.getInt(R.styleable.SetGameCard_symbol_color, 0);
+            mCount = attributes.getInt(R.styleable.SetGameCard_symbol_count, 0);
+            mFill = attributes.getInt(R.styleable.SetGameCard_symbol_fill, 0);
+        } finally {
+            attributes.recycle();
+        }
 
-        attributes.recycle();
+        /**
+         * Registers a listener to fire once the initial layout pass is done so we can
+         * add a child LinearLayout and ImageView and have access to their measure height and width
+         */
+        final FrameLayout view = this; // Create a final reference to this view
+        final Context myContext = context; // Create a final reference to this context
+        view.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                // Immediately unregister the listener so it doesn't fire repeatedly
+                view.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+                /**
+                 * Add a LinearLayout to the card's frame layout
+                 */
+                LinearLayout linearLayout = new LinearLayout(myContext);
+                linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+                linearLayout.setGravity(Gravity.CENTER);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                );
+                linearLayout.setLayoutParams(layoutParams);
+
+                // Add the blank LinearLayout to our card
+                view.addView(linearLayout);
+
+                // Add an image to the LinearLayout in the card
+                addImage(myContext, linearLayout);
+            }
+        });
+
+
     }
 
+    /**
+     * Adds the appropriate symbol image to the center of the card
+     * @param context Context of our view
+     */
+    private void addImage(Context context, LinearLayout linearLayout){
+
+        ImageView symbolView = new ImageView(context);
+
+        // Set the height of the image to 1/2 the card height
+        int imageHeight = getHeight() / 2;
+        LayoutParams params = new LayoutParams(
+                LayoutParams.WRAP_CONTENT,
+                imageHeight
+        );
+
+        symbolView.setLayoutParams(params);
+
+
+        symbolView.setImageDrawable(context.getDrawable(R.drawable.ic_oval_one_open));
+        //Symbol.setMaxHeight(this.getHeight() / 2);
+
+        linearLayout.addView(symbolView);
+    }
 
 
     /**
-     * When the view is measured modify it's width and height to match the aspect ratio
-     * specified in the attributes
-     * @param widthMeasureSpec
-     * @param heightMeasureSpec
+     * Custom aspect ratios have proven difficult to work right. Implement feature later.
      */
-    @Override protected void onMeasure (int widthMeasureSpec, int heightMeasureSpec)
-    {
-        int originalWidth = MeasureSpec.getSize(widthMeasureSpec);
+//    /**
+//     * When the view is measured modify it's height to match the aspect ratio
+//     * specified in the attributes.
+//     * @param widthMeasureSpec The horizontal space requirements imposed by the parent
+//     * @param heightMeasureSpec The vertical space requirements imposed by the parent
+//     */
+//    @Override protected void onMeasure (int widthMeasureSpec, int heightMeasureSpec)
+//    {
+//        int finalWidth, finalHeight;
+//
+//        // Get original height and width
+//        int originalWidth = MeasureSpec.getSize(widthMeasureSpec);
+//        int originalHeight = MeasureSpec.getSize(heightMeasureSpec);
+//
+//        if ( mAspectRatioWidth > mAspectRatioHeight ){
+//            // Scale height based on width
+//            finalHeight = originalWidth * mAspectRatioHeight / mAspectRatioWidth;
+//            finalWidth = originalWidth;
+//        } else if ( mAspectRatioWidth < mAspectRatioHeight ){
+//            // Scale width based on height
+//            finalWidth = originalHeight * mAspectRatioHeight / mAspectRatioWidth;
+//            finalHeight = originalHeight;
+//        } else {
+//            finalWidth = originalWidth;
+//            finalHeight = originalHeight;
+//        }
+//
+//        super.onMeasure(
+//                MeasureSpec.makeMeasureSpec(finalWidth, MeasureSpec.EXACTLY),
+//                MeasureSpec.makeMeasureSpec(finalHeight, MeasureSpec.EXACTLY));
+//    }
 
-        int originalHeight = MeasureSpec.getSize(heightMeasureSpec);
+    /**
+     * Setters and getters for custom attributes
+     */
+//    public int getAspectRatioWidth() {
+//        return mAspectRatioWidth;
+//    }
+//
+//    public void setAspectRatioWidth(int mAspectRatioWidth) {
+//        this.mAspectRatioWidth = mAspectRatioWidth;
+//        invalidate();
+//        requestLayout();
+//    }
+//
+//    public int getAspectRatioHeight() {
+//        return mAspectRatioHeight;
+//    }
+//
+//    public void setAspectRatioHeight(int mAspectRatioHeight) {
+//        this.mAspectRatioHeight = mAspectRatioHeight;
+//        invalidate();
+//        requestLayout();    }
 
-        int calculatedHeight = originalWidth * mAspectRatioHeight / mAspectRatioWidth;
-
-        int finalWidth, finalHeight;
-
-        if (calculatedHeight > originalHeight)
-        {
-            finalWidth = originalHeight * mAspectRatioWidth / mAspectRatioHeight;
-            finalHeight = originalHeight;
-        }
-        else
-        {
-            finalWidth = originalWidth;
-            finalHeight = calculatedHeight;
-        }
-
-        // Pass back the modified width and height.
-        super.onMeasure(
-                MeasureSpec.makeMeasureSpec(finalWidth, MeasureSpec.EXACTLY),
-                MeasureSpec.makeMeasureSpec(finalHeight, MeasureSpec.EXACTLY));
+    public int getShape() {
+        return mShape;
     }
 
+    public void setShape(int mShape) {
+        this.mShape = mShape;
+        invalidate();
+        requestLayout();
+    }
+
+    public int getColor() {
+        return mColor;
+    }
+
+    public void setColor(int mColor) {
+        this.mColor = mColor;
+        invalidate();
+        requestLayout();
+    }
+
+    public int getCount() {
+        return mCount;
+    }
+
+    public void setCount(int mCount) {
+        this.mCount = mCount;
+        invalidate();
+        requestLayout();
+    }
+
+    public int getFill() {
+        return mFill;
+    }
+
+    public void setFill(int mFill) {
+        this.mFill = mFill;
+        invalidate();
+        requestLayout();
+    }
 }
