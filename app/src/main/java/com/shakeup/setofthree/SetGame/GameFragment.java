fe51b0a739d4ba0cef1ee373fb8878cce056f179
Jayson Dela Cruz <jayson.p.delacruz@gmail.com>
2017-03-14 15:20:10 -0700

    Cards in the gridview are now selectable.

--- app/src/main/java/com/shakeup/setofthree/SetGame/GameFragment.java
+++ app/src/main/java/com/shakeup/setofthree/SetGame/GameFragment.java
@@ -3,6 +3,8 @@ package com.shakeup.setofthree.SetGame;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,10 +19,14 @@ import java.util.ArrayList;
 
 /**
  * Created by Jayson on 3/2/2017.
+ *
+ * Main fragment for playing a game of SET. May be subclassed to implmeent
+ * variants of the main game
  */
 
 
-public class GameFragment extends Fragment implements GameContract.View {
+public class GameFragment extends Fragment
+        implements GameContract.View, SetGameGridCallback {
 
     // Listener for presenter to handle all user input
     private GamePresenter mActionsListener;
@@ -30,6 +36,11 @@ public class GameFragment extends Fragment implements GameContract.View {
 
     // Adapter containing the current Set Hand displayed on the board
     private SetGameGridAdapter mGridAdapter;
+
+    // Adapter to hold this fragment as a usable context
+    private SetGameGridCallback mGameFragment;
+
+    private String LOG_TAG = this.getClass().getSimpleName();
 
     /**
      * Allow another class to construct us
@@ -44,11 +55,17 @@ public class GameFragment extends Fragment implements GameContract.View {
     // Run initial setup for creating a new game
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(
+            LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_game, container, false);
 
         // Instance the presenter our fragment uses
         mActionsListener = new GamePresenter(this);
+
+        // Keep an instance of this fragment to be used by subclasses
+        mGameFragment = this;
 
         // Grab handlers for UI elements
         mGridView = (GridView) root.findViewById(R.id.game_grid);
@@ -76,8 +93,47 @@ public class GameFragment extends Fragment implements GameContract.View {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 SetGameCard card = (SetGameCard) view;
                 card.toggle();
+
+                // Let the fragment know we've been clicked
+                mGameFragment.onSetCardClicked();
             }
         });
+
     }
 
+
+    /**
+     * This is called whenever a SET card is clicked by the user.
+     * If 3 cards are selected those indices are sent to the presenter
+     * who will check if they are a valid set.
+     */
+    @Override
+    public void onSetCardClicked() {
+        Log.d("TEST", mGridView.getCheckedItemCount() + " items selected.");
+
+        // If we have 3 items selected, check if they are a set
+        if (mGridView.getCheckedItemCount() == 3){
+            SparseBooleanArray checkedItemPositions = mGridView.getCheckedItemPositions();
+
+            int positionIndex = 0;
+            int[] positions = new int[3];
+
+            // Loop through SparseBooleanArray and grab the 3 positions that are checked
+            for( int i = 0; i < checkedItemPositions.size() ; i++ ){
+                if( checkedItemPositions.valueAt(i) ){
+                    positions[positionIndex] = checkedItemPositions.keyAt(i);
+                    positionIndex++;
+                }
+            }
+
+            Log.d(LOG_TAG, String.format(
+                    "Checked %d, %d, %d",
+                    positions[0],
+                    positions[1],
+                    positions[2]));
+
+            // Clear all selections from GridView
+            mGridView.clearChoices();
+        }
+    }
 }
---
+++ app/src/main/java/com/shakeup/setofthree/SetGame/SetGameGridCallback.java
@@ -0,0 +1,12 @@
+package com.shakeup.setofthree.SetGame;
+
+/**
+ * Created by Jayson on 3/14/2017.
+ *
+ * The game fragment must implement this interface so we can handle
+ * actions after each SET card is clicked
+ */
+
+public interface SetGameGridCallback {
+    void onSetCardClicked();
+}
